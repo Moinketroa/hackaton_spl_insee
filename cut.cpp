523c8332ca6a2988fbb7ad004b3ef3e87d6f0a5c
Moinketroa <jean-marc.debicki1@etu.univ-lorraine.fr>
2016-03-17 20:03:30 +0100

    ajout des fichiers coupés

---
+++ cut.cpp
@@ -0,0 +1,24 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio>
+
+int main(int argc, char** argv){
+	int nline = 10000;
+	std::ifstream in;
+	in.open( argv[1], std::ifstream::in );
+	std::ofstream out;
+	std::string line;
+	int filenum = 0;
+	for (int i=0; std::getline(in, line); i++){
+		if (i%nline == 0){
+			out.close();
+			filenum ++;
+			char name[25];
+			sprintf(name, "part%d.sql", filenum);
+			out.open( name );
+		}
+		out << line << "\n";
+	}
+}
