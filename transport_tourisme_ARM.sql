0ecdbab5c9bd37b9632cba94ccad44daf5f905e1
hackathon <hackathon@debian>
2016-03-17 15:22:29 +0100

    fichiers sql

---
+++ transport_tourisme_ARM.sql
@@ -0,0 +1,61 @@
+CREATE TABLE transport_tourisme_ARM (
+    CODGEO INT,
+    LIBGEO VARCHAR(28) CHARACTER SET utf8,
+    DEP INT,
+    REG INT,
+    REG2016 INT,
+    TAXI NUMERIC(4, 1),
+    AERO NUMERIC(2, 1),
+    GARE_TGV NUMERIC(2, 1),
+    GARE_ETAT NUMERIC(2, 1),
+    GARE_CR_STIF NUMERIC(2, 1),
+    AG_VOY NUMERIC(4, 1),
+    HOTEL NUMERIC(4, 1),
+    CAMP NUMERIC(2, 1),
+    INF_TOUR NUMERIC(3, 1)
+);
+INSERT INTO transport_tourisme_ARM VALUES (13201,'Marseille 1er Arrondissement',13,93,93,9.0,0.0,1.0,0.0,0.0,53.0,25.0,0.0,11.0);
+INSERT INTO transport_tourisme_ARM VALUES (13202,'Marseille 2e Arrondissement',13,93,93,15.0,0.0,0.0,0.0,0.0,13.0,10.0,0.0,3.0);
+INSERT INTO transport_tourisme_ARM VALUES (13203,'Marseille 3e Arrondissement',13,93,93,17.0,0.0,0.0,0.0,0.0,6.0,0.0,0.0,3.0);
+INSERT INTO transport_tourisme_ARM VALUES (13204,'Marseille 4e Arrondissement',13,93,93,32.0,0.0,0.0,0.0,1.0,4.0,1.0,0.0,1.0);
+INSERT INTO transport_tourisme_ARM VALUES (13205,'Marseille 5e Arrondissement',13,93,93,30.0,0.0,0.0,0.0,0.0,6.0,3.0,0.0,2.0);
+INSERT INTO transport_tourisme_ARM VALUES (13206,'Marseille 6e Arrondissement',13,93,93,17.0,0.0,0.0,0.0,0.0,25.0,14.0,0.0,7.0);
+INSERT INTO transport_tourisme_ARM VALUES (13207,'Marseille 7e Arrondissement',13,93,93,20.0,0.0,0.0,0.0,0.0,7.0,10.0,0.0,4.0);
+INSERT INTO transport_tourisme_ARM VALUES (13208,'Marseille 8e Arrondissement',13,93,93,43.0,0.0,0.0,0.0,0.0,22.0,12.0,0.0,17.0);
+INSERT INTO transport_tourisme_ARM VALUES (13209,'Marseille 9e Arrondissement',13,93,93,64.0,0.0,0.0,0.0,0.0,8.0,1.0,0.0,8.0);
+INSERT INTO transport_tourisme_ARM VALUES (13210,'Marseille 10e Arrondissement',13,93,93,66.0,0.0,0.0,0.0,0.0,2.0,2.0,0.0,4.0);
+INSERT INTO transport_tourisme_ARM VALUES (13211,'Marseille 11e Arrondissement',13,93,93,68.0,0.0,0.0,0.0,2.0,3.0,8.0,0.0,1.0);
+INSERT INTO transport_tourisme_ARM VALUES (13212,'Marseille 12e Arrondissement',13,93,93,75.0,0.0,0.0,0.0,0.0,5.0,1.0,0.0,1.0);
+INSERT INTO transport_tourisme_ARM VALUES (13213,'Marseille 13e Arrondissement',13,93,93,97.0,0.0,0.0,0.0,0.0,5.0,0.0,0.0,4.0);
+INSERT INTO transport_tourisme_ARM VALUES (13214,'Marseille 14e Arrondissement',13,93,93,50.0,0.0,0.0,0.0,2.0,7.0,0.0,0.0,0.0);
+INSERT INTO transport_tourisme_ARM VALUES (13215,'Marseille 15e Arrondissement',13,93,93,52.0,0.0,0.0,0.0,4.0,10.0,1.0,0.0,3.0);
+INSERT INTO transport_tourisme_ARM VALUES (13216,'Marseille 16e Arrondissement',13,93,93,7.0,0.0,0.0,0.0,2.0,13.0,3.0,0.0,1.0);
+INSERT INTO transport_tourisme_ARM VALUES (69381,'Lyon 1er Arrondissement',69,82,84,10.0,0.0,0.0,0.0,0.0,16.0,7.0,0.0,7.0);
+INSERT INTO transport_tourisme_ARM VALUES (69382,'Lyon 2e Arrondissement',69,82,84,14.0,0.0,1.0,0.0,0.0,49.0,43.0,0.0,5.0);
+INSERT INTO transport_tourisme_ARM VALUES (69383,'Lyon 3e Arrondissement',69,82,84,44.0,0.0,1.0,0.0,0.0,53.0,19.0,0.0,5.0);
+INSERT INTO transport_tourisme_ARM VALUES (69384,'Lyon 4e Arrondissement',69,82,84,13.0,0.0,0.0,0.0,0.0,8.0,4.0,0.0,5.0);
+INSERT INTO transport_tourisme_ARM VALUES (69385,'Lyon 5e Arrondissement',69,82,84,24.0,0.0,0.0,0.0,1.0,4.0,6.0,0.0,4.0);
+INSERT INTO transport_tourisme_ARM VALUES (69386,'Lyon 6e Arrondissement',69,82,84,10.0,0.0,0.0,0.0,0.0,21.0,11.0,0.0,6.0);
+INSERT INTO transport_tourisme_ARM VALUES (69387,'Lyon 7e Arrondissement',69,82,84,32.0,0.0,0.0,0.0,1.0,29.0,9.0,0.0,12.0);
+INSERT INTO transport_tourisme_ARM VALUES (69388,'Lyon 8e Arrondissement',69,82,84,54.0,0.0,0.0,0.0,0.0,15.0,4.0,0.0,4.0);
+INSERT INTO transport_tourisme_ARM VALUES (69389,'Lyon 9e Arrondissement',69,82,84,30.0,0.0,0.0,0.0,2.0,15.0,3.0,0.0,7.0);
+INSERT INTO transport_tourisme_ARM VALUES (75101,'Paris 1er Arrondissement',75,11,11,27.0,0.0,0.0,0.0,0.0,93.0,72.0,0.0,44.0);
+INSERT INTO transport_tourisme_ARM VALUES (75102,'Paris 2e Arrondissement',75,11,11,11.0,0.0,0.0,0.0,0.0,112.0,36.0,0.0,28.0);
+INSERT INTO transport_tourisme_ARM VALUES (75103,'Paris 3e Arrondissement',75,11,11,19.0,0.0,0.0,0.0,0.0,38.0,27.0,0.0,34.0);
+INSERT INTO transport_tourisme_ARM VALUES (75104,'Paris 4e Arrondissement',75,11,11,9.0,0.0,0.0,0.0,1.0,30.0,33.0,0.0,21.0);
+INSERT INTO transport_tourisme_ARM VALUES (75105,'Paris 5e Arrondissement',75,11,11,79.0,0.0,0.0,0.0,0.0,57.0,80.0,0.0,24.0);
+INSERT INTO transport_tourisme_ARM VALUES (75106,'Paris 6e Arrondissement',75,11,11,12.0,0.0,0.0,0.0,0.0,44.0,104.0,0.0,19.0);
+INSERT INTO transport_tourisme_ARM VALUES (75107,'Paris 7e Arrondissement',75,11,11,32.0,0.0,0.0,0.0,4.0,30.0,64.0,0.0,28.0);
+INSERT INTO transport_tourisme_ARM VALUES (75108,'Paris 8e Arrondissement',75,11,11,124.0,0.0,0.0,1.0,0.0,183.0,140.0,0.0,66.0);
+INSERT INTO transport_tourisme_ARM VALUES (75109,'Paris 9e Arrondissement',75,11,11,76.0,0.0,0.0,0.0,1.0,171.0,171.0,0.0,49.0);
+INSERT INTO transport_tourisme_ARM VALUES (75110,'Paris 10e Arrondissement',75,11,11,93.0,0.0,2.0,0.0,1.0,117.0,118.0,0.0,58.0);
+INSERT INTO transport_tourisme_ARM VALUES (75111,'Paris 11e Arrondissement',75,11,11,116.0,0.0,0.0,0.0,0.0,93.0,68.0,0.0,50.0);
+INSERT INTO transport_tourisme_ARM VALUES (75112,'Paris 12e Arrondissement',75,11,11,123.0,0.0,1.0,0.0,1.0,67.0,71.0,0.0,42.0);
+INSERT INTO transport_tourisme_ARM VALUES (75113,'Paris 13e Arrondissement',75,11,11,208.0,0.0,0.0,1.0,1.0,64.0,43.0,0.0,73.0);
+INSERT INTO transport_tourisme_ARM VALUES (75114,'Paris 14e Arrondissement',75,11,11,94.0,0.0,0.0,0.0,0.0,49.0,98.0,0.0,46.0);
+INSERT INTO transport_tourisme_ARM VALUES (75115,'Paris 15e Arrondissement',75,11,11,210.0,0.0,1.0,0.0,2.0,109.0,95.0,0.0,78.0);
+INSERT INTO transport_tourisme_ARM VALUES (75116,'Paris 16e Arrondissement',75,11,11,116.0,0.0,0.0,0.0,4.0,104.0,71.0,1.0,75.0);
+INSERT INTO transport_tourisme_ARM VALUES (75117,'Paris 17e Arrondissement',75,11,11,161.0,0.0,0.0,0.0,4.0,132.0,117.0,0.0,72.0);
+INSERT INTO transport_tourisme_ARM VALUES (75118,'Paris 18e Arrondissement',75,11,11,249.0,0.0,0.0,0.0,0.0,80.0,58.0,0.0,87.0);
+INSERT INTO transport_tourisme_ARM VALUES (75119,'Paris 19e Arrondissement',75,11,11,240.0,0.0,0.0,0.0,0.0,50.0,20.0,0.0,50.0);
+INSERT INTO transport_tourisme_ARM VALUES (75120,'Paris 20e Arrondissement',75,11,11,256.0,0.0,0.0,0.0,0.0,45.0,18.0,0.0,55.0);
---
+++ transport_tourisme_VAR.sql
@@ -0,0 +1,22 @@
+CREATE TABLE transport_tourisme_VAR (
+    VAR_ID VARCHAR(12) CHARACTER SET utf8,
+    VAR_LIB VARCHAR(60) CHARACTER SET utf8,
+    VAR_LIB_LONG VARCHAR(355) CHARACTER SET utf8
+);
+INSERT INTO transport_tourisme_VAR VALUES ('CODGEO','Département -commune',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('LIBGEO','Libellé de la commune',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('DEP','Département',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('REG','Région',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('REG2016','Région 2016',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('TAXI','Taxi',NULL);
+INSERT INTO transport_tourisme_VAR VALUES ('AERO','Aéroport','Aéroport en activité avec plus de 1 000 passagers (mouvement commercial au départ ou à l’arrivée de l’aéroport hors transit).');
+INSERT INTO transport_tourisme_VAR VALUES ('GARE_TGV','Gare avec train TAGV (train à grande vitesse)','Gare desservie par au moins un train à grande vitesse');
+INSERT INTO transport_tourisme_VAR VALUES ('GARE_ETAT','Gare sous convention avec l’État','Gare desservie par au moins un train sous convention avec l’État (sans TAGV)');
+INSERT INTO transport_tourisme_VAR VALUES ('GARE_CR_STIF','Gare sous convention avec les conseils régionaux ou les STIF','Gare desservie par au moins un train sous convention avec les conseils régionaux ou les STIF (transport Île de France) (sans TAGV et sans convention État)');
+INSERT INTO transport_tourisme_VAR VALUES ('AG_VOY','Agence de voyage','Agences de voyage et voyagistes
+Ne comprend plus les services de réservation et d''information touristique suite au passage à la NAF rév.2. Différence de -2 800 à compter de 2009.');
+INSERT INTO transport_tourisme_VAR VALUES ('HOTEL','Hôtel homologué','Hôtels homologués, auxquels sont rajoutés les hôtels de chaîne à 0 étoile. Ne sont donc pas pris en compte les anciens "hôtels de préfecture".
+Avec ou sans restaurant.');
+INSERT INTO transport_tourisme_VAR VALUES ('CAMP','Camping homologué','Campings homologués, classés de 0 à 4 étoiles.  Donc y compris campings des comités d''entreprise, à clientèle spécifique (ouvriers, colonies de vacances, forains…) si  cette clientèle est de passage - c''est à dire ne restant pas pendant toute la période d''ouverture du camping - et campings de VVF. 
+Non compris services de réservation et activités liées ');
+INSERT INTO transport_tourisme_VAR VALUES ('INF_TOUR','Information Touristique','Comprend les services de réservation et d''information touristique.');
