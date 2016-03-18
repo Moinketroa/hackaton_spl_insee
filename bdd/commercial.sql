f5425403e6e491c1bc744a16cd6aaa9f4bea98bb
root <root@debian>
2016-03-18 23:12:24 +0100

    ajout des sql (non complet)

---
+++ bdd/commercial.sql
@@ -0,0 +1,33 @@
+CREATE TABLE commercial (
+    type VARCHAR(10) CHARACTER SET utf8,
+    nom VARCHAR(37) CHARACTER SET utf8,
+    x NUMERIC(17, 16),
+    y NUMERIC(16, 14),
+    z INT
+);
+INSERT INTO commercial VALUES ('Commercial','Saint Sébastien',6.18120399999998,48.687957,0);
+INSERT INTO commercial VALUES ('Commercial','Centre Commercial Auchan La Sapinière',6.125536000000011,48.691918,0);
+INSERT INTO commercial VALUES ('Commercial','Auchan Laxou',6.12547799999993,48.691767,0);
+INSERT INTO commercial VALUES ('Commercial','Auchan Tomblaine',6.22184500000003,48.684812,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarché Match',6.16888229999995,48.6673159,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarché Match',6.153344000000061,48.6722741,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.179193700000042,48.68842979999999,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarché Match',6.16888229999995,48.6673159,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.160744000000023,48.6855647,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.174588299999982,48.6625605,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.134413900000027,48.70023,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.213523600000031,48.7021332,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.162305500000002,48.7364509,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.296937000000072,48.6338251,0);
+INSERT INTO commercial VALUES ('Commercial','Supermarchés Match',6.352017100000013,48.6097133,0);
+INSERT INTO commercial VALUES ('Commercial','Centre Commercial Les Nations',6.174932799999965,48.6621349,0);
+INSERT INTO commercial VALUES ('Commercial','Saveurs d''Orient',6.183498999999983,48.659538,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour Express',6.179276500000015,48.69587720000001,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour City',6.183829699999933,48.6910918,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour Express',6.1755865000000085,48.68935570000001,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour City',6.18459699999994,48.68964499999999,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour City',6.173054099999945,48.67197509999999,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour City',6.1864691000000676,48.6842457,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour Express Nancy Boufflers',6.161750299999994,48.68937709999999,0);
+INSERT INTO commercial VALUES ('Commercial','Carrefour Contact',6.172495799999979,48.6838403,0);
+INSERT INTO commercial VALUES ('Commercial','U Express',6.165610000000015,48.6660528,0);
