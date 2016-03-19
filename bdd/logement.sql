668a207e363fa81effb23cd837e6fd8c86853aa9
root <root@debian>
2016-03-19 04:13:33 +0100

    database

--- bdd/logement.sql
+++ bdd/logement.sql
@@ -1,30 +1,31 @@
 CREATE TABLE logement (
-    log_id INT AUTO INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     type VARCHAR(10) CHARACTER SET utf8,
     nom VARCHAR(48) CHARACTER SET utf8,
     x NUMERIC(17, 16),
     y NUMERIC(17, 15),
-    z INT
+    z INT,
+    PRIMARY KEY (id)
 );
-INSERT INTO logement VALUES ('IMMOBILIER','agence immobilière ORPI',6.18466860000001,48.6880309,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Laforêt Immobilier',6.166913600000044,48.6883096,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Foncia Transaction',6.175987999999961,48.6907321,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Foncia Solonim',6.178120000000035,48.6803296,0);
-INSERT INTO logement VALUES ('IMMOBILIER','CENTURY 21 Joël Pierre Immobilier',6.177341100000035,48.6918187,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Century 21 Midon Baudoin Immobilier',6.2051753000000645,48.6996524,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Laforêt Vandoeuvre Les Nancy',6.167601699999977,48.6677044,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Olma Immobilier VANDOEUVRE LES',6.174976399999991,48.6624355,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Thouvenin Immobilier',6.164420100000029,48.6616749,0);
-INSERT INTO logement VALUES ('IMMOBILIER','Liberty Home',6.187213499999984,48.6851496,0);
-INSERT INTO logement VALUES ('IMMOBILIER','La Une de L''immo (VIAEMEDIA)',6.160781199999974,48.6620097,0);
-INSERT INTO logement VALUES ('IMMOBILIER','FB Immo Conseil',6.174932799999965,48.6621349,0);
-INSERT INTO logement VALUES ('IMMOBILIER','IMMOLIVIER',6.173760000000016,48.661118,0);
-INSERT INTO logement VALUES ('ResidenceU','Residence Universitaire Charmois',6.173686900000007,48.6671674,0);
-INSERT INTO logement VALUES ('ResidenceU','Residence Monbois',6.162809799999991,48.696657800000004,0);
-INSERT INTO logement VALUES ('ResidenceU','Residence Universitaire Proveneal',6.194953400000031,48.67713570000001,0);
-INSERT INTO logement VALUES ('ResidenceU','Cite Universitaire Monplaisir',6.1630826000000525,48.6654651,0);
-INSERT INTO logement VALUES ('ResidenceU','Le Crous de Nancy-Metz',6.1584814999999935,48.6845541,0);
-INSERT INTO logement VALUES ('ResidenceU','Centre Regional Oeuvres Universitaires Scolaires',6.162814799999978,48.6964505,0);
-INSERT INTO logement VALUES ('ResidenceU','Residence Universitaire Placieux',6.156414799999993,48.6725262,0);
-INSERT INTO logement VALUES ('ResidenceU','Cite Universitaire Haute-Malgrange',6.189140400000042,48.660777,0);
-INSERT INTO logement VALUES ('ResidenceU','Residence Bateliere',6.167493199999968,48.7058791,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','agence immobilière ORPI',6.18466860000001,48.6880309,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Laforêt Immobilier',6.166913600000044,48.6883096,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Foncia Transaction',6.175987999999961,48.6907321,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Foncia Solonim',6.178120000000035,48.6803296,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','CENTURY 21 Joël Pierre Immobilier',6.177341100000035,48.6918187,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Century 21 Midon Baudoin Immobilier',6.2051753000000645,48.6996524,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Laforêt Vandoeuvre Les Nancy',6.167601699999977,48.6677044,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Olma Immobilier VANDOEUVRE LES',6.174976399999991,48.6624355,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Thouvenin Immobilier',6.164420100000029,48.6616749,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','Liberty Home',6.187213499999984,48.6851496,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','La Une de L''immo (VIAEMEDIA)',6.160781199999974,48.6620097,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','FB Immo Conseil',6.174932799999965,48.6621349,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('IMMOBILIER','IMMOLIVIER',6.173760000000016,48.661118,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Residence Universitaire Charmois',6.173686900000007,48.6671674,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Residence Monbois',6.162809799999991,48.696657800000004,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Residence Universitaire Proveneal',6.194953400000031,48.67713570000001,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Cite Universitaire Monplaisir',6.1630826000000525,48.6654651,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Le Crous de Nancy-Metz',6.1584814999999935,48.6845541,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Centre Regional Oeuvres Universitaires Scolaires',6.162814799999978,48.6964505,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Residence Universitaire Placieux',6.156414799999993,48.6725262,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Cite Universitaire Haute-Malgrange',6.189140400000042,48.660777,0);
+INSERT INTO logement (type, nom, x, y, z) VALUES ('ResidenceU','Residence Bateliere',6.167493199999968,48.7058791,0);
--- bdd/sante.sql
+++ bdd/sante.sql
@@ -1,28 +1,29 @@
 CREATE TABLE sante (
-    sa_id INT AUTO INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     type VARCHAR(8) CHARACTER SET utf8,
     nom VARCHAR(53) CHARACTER SET utf8,
     x NUMERIC(16, 15),
     y NUMERIC(17, 15),
-    z INT
+    z INT,
+    PRIMARY KEY (id)
 );
-INSERT INTO sante VALUES ('HOPITAL','CHRU Nancy',6.192021999999952,48.6852009,0);
-INSERT INTO sante VALUES ('HOPITAL','Bâtiment Brabois Adultes',6.146791600000029,48.64761919999999,0);
-INSERT INTO sante VALUES ('HOPITAL','CHU de Nancy - Hôpitaux de Brabois',6.147823799999969,48.646989,0);
-INSERT INTO sante VALUES ('HOPITAL','Bâtiment des Spécialités Médicales Philippe Canton',6.147252900000012,48.6458497,0);
-INSERT INTO sante VALUES ('HOPITAL','Heliport Hopital Central',6.192477999999937,48.68542699999999,0);
-INSERT INTO sante VALUES ('HOPITAL','Polyclinique de Gentilly',6.139275999999995,48.7018589,0);
-INSERT INTO sante VALUES ('HOPITAL','Institut de Cancérologie de Lorraine - Alexis Vautrin',6.144856300000015,48.64808569999999,0);
-INSERT INTO sante VALUES ('HOPITAL','Clinique Louis Pasteur',6.218064899999945,48.7015192,0);
-INSERT INTO sante VALUES ('HOPITAL','Clinique Jeanne d''Arc',6.175420499999973,48.680872699999995,0);
-INSERT INTO sante VALUES ('HOPITAL','Polyclinique Majorelle',6.144168000000036,48.700768,0);
-INSERT INTO sante VALUES ('HOPITAL','Clinique Ambroise Paré',6.141480000000002,48.70247819999999,0);
-INSERT INTO sante VALUES ('RETRAITE','Maison de Retraite ORPEA Les Cygnes',6.181739999999991,48.704736,0);
-INSERT INTO sante VALUES ('RETRAITE','Korian Plaisance',6.192304000000036,48.69314199999999,0);
-INSERT INTO sante VALUES ('RETRAITE','Korian Le Gentile',6.130307000000016,48.6960528,0);
-INSERT INTO sante VALUES ('RETRAITE','Korian Le Haut du Bois',6.20681680000007,48.6590657,0);
-INSERT INTO sante VALUES ('RETRAITE','Maison de retraite Saint Remy',6.170222100000046,48.6996763,0);
-INSERT INTO sante VALUES ('RETRAITE','Korian La Saulx',6.12332760000004,48.69776599999999,0);
-INSERT INTO sante VALUES ('RETRAITE','Maison de Retraite du Bas Château',6.216840000000047,48.70778700000001,0);
-INSERT INTO sante VALUES ('RETRAITE','La Maison des Vignes, ADEF Résidences',6.17531919999999,48.71483569999999,0);
-INSERT INTO sante VALUES ('RETRAITE','Korian Jardins du Charmois',6.170978500000047,48.66860260000001,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','CHRU Nancy',6.192021999999952,48.6852009,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Bâtiment Brabois Adultes',6.146791600000029,48.64761919999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','CHU de Nancy - Hôpitaux de Brabois',6.147823799999969,48.646989,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Bâtiment des Spécialités Médicales Philippe Canton',6.147252900000012,48.6458497,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Heliport Hopital Central',6.192477999999937,48.68542699999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Polyclinique de Gentilly',6.139275999999995,48.7018589,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Institut de Cancérologie de Lorraine - Alexis Vautrin',6.144856300000015,48.64808569999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Clinique Louis Pasteur',6.218064899999945,48.7015192,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Clinique Jeanne d''Arc',6.175420499999973,48.680872699999995,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Polyclinique Majorelle',6.144168000000036,48.700768,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('HOPITAL','Clinique Ambroise Paré',6.141480000000002,48.70247819999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Maison de Retraite ORPEA Les Cygnes',6.181739999999991,48.704736,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Korian Plaisance',6.192304000000036,48.69314199999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Korian Le Gentile',6.130307000000016,48.6960528,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Korian Le Haut du Bois',6.20681680000007,48.6590657,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Maison de retraite Saint Remy',6.170222100000046,48.6996763,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Korian La Saulx',6.12332760000004,48.69776599999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Maison de Retraite du Bas Château',6.216840000000047,48.70778700000001,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','La Maison des Vignes, ADEF Résidences',6.17531919999999,48.71483569999999,0);
+INSERT INTO sante (type, nom, x, y, z) VALUES ('RETRAITE','Korian Jardins du Charmois',6.170978500000047,48.66860260000001,0);
