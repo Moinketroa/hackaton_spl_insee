3213db571ae3e309a93a380403dfd44b665b353d
root <root@debian>
2016-03-19 08:15:23 +0100

    php

---
+++ bdd/ecole.sql
@@ -0,0 +1,34 @@
+CREATE TABLE ecole (
+    id INT NOT NULL AUTO_INCREMENT,
+    type VARCHAR(10) CHARACTER SET utf8,
+    nom VARCHAR(74) CHARACTER SET utf8,
+    x NUMERIC(17, 16),
+    y NUMERIC(16, 14),
+    z INT,
+    PRIMARY KEY (id)
+);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée Stanislas',6.152387800000042,48.6627853,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée Henri Poincaré',6.179624800000056,48.69139819999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée Henri Loritz',6.191807299999937,48.6902195,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée Régional Arthur Varoquaux',6.21787500000005,48.697447,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée Frédéric Chopin',6.167643099999964,48.68050579999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycée d''Enseignement Général et Technologique Agricole Mathieu de Dombasle',6.180753800000048,48.7296752,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Collège-Lycée Claude Daunot',6.180366000000049,48.677737,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycee D''Enseignement Professionnel Marie Marvingt',6.21572900000001,48.6960202,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycee Charles de Foucauld',6.188010399999939,48.6899039,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('LYCEE','Lycee Jeanne d''Arc',6.185888900000009,48.69324169999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','College Jean de La Fontaine',6.134756700000025,48.69911279999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','College Jean Moulin',6.2137467000000015,48.6956638,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','Collège Edmond de Goncourt',6.261167500000057,48.7060938,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','Collège Victor Prouvé',6.143669499999987,48.67771740000001,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','Collège du Sacré Coeur',6.162136299999929,48.6862123,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','Lycée Frédéric Chopin',6.167643099999964,48.68050579999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','College Paul Verlaine',6.1795629000000645,48.72349939999999,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecoles Maternelles Publiques',6.156762100000037,48.7068872,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Maternelle Boudonville',6.163651399999935,48.6980539,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Preelementaire Albert Schweitzer',6.123349899999994,48.6982494,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Maternelle Suzanne Herbinière-Lebert',6.155413400000043,48.6730815,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Maternelle Saint-Pierre',6.192071299999952,48.682028,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Maternelle Clemenceau',6.207157400000028,48.7017619,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('MATERNELLE','Ecole Maternelle Tiercelins',6.189139000000068,48.6897688,0);
+INSERT INTO ecole (type, nom, x, y, z) VALUES ('COLLEGE','Collège-Lycée Claude Daunot',6.180366000000049,48.677737,0);
